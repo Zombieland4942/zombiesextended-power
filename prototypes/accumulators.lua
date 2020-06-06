1279216fd2af520b92731678a376c679f8a0a71c
Zombieland4942 <zombieland4942@hotmail.com>
2020-06-06 11:17:13 +1000

    Cleaning up some code

--- prototypes/accumulators.lua
+++ prototypes/accumulators.lua
@@ -1,22 +1,22 @@
 
-local accumulator_base = util.table.deepcopy(data.raw["accumulator"]["accumulator"])
+local entity_base = util.table.deepcopy(data.raw["accumulator"]["accumulator"])
 local item_base = util.table.deepcopy(data.raw["item"]["accumulator"])
 
 for x, accumulator in pairs(accumulators) do
     
-    accumul = util.table.deepcopy(accumulator_base)
+    entity = util.table.deepcopy(entity_base)
     item = util.table.deepcopy(item_base)
 
-    accumul.name = accumulator.name    
-    accumul.icon = "__darkstar-power__/graphics/icons/" .. accumulator.name .. ".png"
-    accumul.minable.result = accumulator.name
-    accumul.max_health = accumulator.health
-    accumul.energy_source.buffer_capacity  = accumulator.buffer_mj .. "MJ"
-    accumul.energy_source.input_flow_limit  = accumulator.flow_in_kw .. "kW"
-    accumul.energy_source.output_flow_limit  = accumulator.flow_out_kw .. "kW"
+    entity.name = accumulator.name    
+    entity.icon = "__darkstar-power__/graphics/icons/" .. accumulator.name .. ".png"
+    entity.minable.result = accumulator.name
+    entity.max_health = accumulator.health
+    entity.energy_source.buffer_capacity  = accumulator.buffer_mj .. "MJ"
+    entity.energy_source.input_flow_limit  = accumulator.flow_in_kw .. "kW"
+    entity.energy_source.output_flow_limit  = accumulator.flow_out_kw .. "kW"
     
-    accumul.picture.layers[1].filename = "__darkstar-power__/graphics/entity/" .. accumulator.name .. "/accumulator.png"
-    accumul.picture.layers[1].hr_version.filename = "__darkstar-power__/graphics/entity/" .. accumulator.name .. "/hr-accumulator.png"
+    entity.picture.layers[1].filename = "__darkstar-power__/graphics/entity/" .. accumulator.name .. "/accumulator.png"
+    entity.picture.layers[1].hr_version.filename = "__darkstar-power__/graphics/entity/" .. accumulator.name .. "/hr-accumulator.png"
 
     item.name = accumulator.name
     item.icon = "__darkstar-power__/graphics/icons/" .. accumulator.name .. ".png"
@@ -26,7 +26,7 @@ for x, accumulator in pairs(accumulators) do
     
     table.insert(data.raw["technology"][accumulator.technology].effects, { type = "unlock-recipe", recipe = accumulator.name })
 
-    data:extend({ accumul, item,
+    data:extend({ entity, item,
     {
         type = "recipe",
         name = accumulator.name,
