a6f322e1e8e69ee33188a8712053d70ed1a75214
Zombieland4942 <zombieland4942@hotmail.com>
2021-04-14 19:06:22 +1000

    - Added a fix so that the mod loads with warptorio2_expansion

---
+++ data-final-fixes.lua
@@ -0,0 +1,5 @@
+
+if mods["warptorio2_expansion"] then
+    data.raw["electric-pole"]["wpe_leak_pole"].fast_replaceable_group = nil
+    data.raw["electric-pole"]["wpe_leak_pole"].next_upgrade = nil
+end
